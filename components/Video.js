347f9100d7674b46829ed7fbc3894709ce782711
tobiakinlade <tobi.akinlade@yahoo.com>
2022-07-02 06:21:33 +0100

    list videos on homepage

--- components/Video.js
+++ components/Video.js
@@ -1,5 +1,54 @@
-import React from 'react'
+import timeago from 'lib/timeago'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Video({ video }) {
-  return <p className='text-lg font-bold text-white'>{video.title}</p>
+  return (
+    <div className=''>
+      <div className='px-5 pb-5'>
+        {video.thumbnail && (
+          <Link href={`/video/${video.id}`}>
+            <Image
+              className='mb-2 cursor-pointer'
+              src={video.thumbnail}
+              width='800'
+              height='450'
+            />
+          </Link>
+        )}
+        <p className='text-white relative float-right mr-1 -mt-11 bg-black p-1'>
+          {Math.floor(video.length / 60)
+            .toString()
+            .padStart(2, '0')}
+          :{(video.length % 60).toString().padStart(2, '0')}
+        </p>
+        <div className='flex'>
+          {video.author.image && (
+            <img
+              className='w-10 h-10 mt-2 mr-2 rounded-full '
+              src={video.author.image}
+            />
+          )}
+
+          <div>
+            <Link href={`/video/${video.id}`}>
+              <a className='text-lg font-bold text-white'>{video.title}</a>
+            </Link>
+
+            <div>
+              <div className='text-gray-400'>
+                <Link href={`/channel/${video.author.username}`}>
+                  <a className='mr-2 underline cursor-pointer'>
+                    {video.author.name}
+                  </a>
+                </Link>
+                . {video.views} views .{' '}
+                {timeago.format(new Date(video.createdAt))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'localhost',
+      'tobi-bootcamp.s3.eu-west-2.amazonaws.com',
+      'tobi-bootcamp.s3.eu-west-2.amazonaws.com',
+    ],
+  },
 }
 
 module.exports = nextConfig
